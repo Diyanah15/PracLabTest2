9bbc767494a4e1fbb988f1854b80e551487d0e4c
Diyanah <ndiyanah.21@ichat.sp.edu.sg>
2022-11-27 19:59:05 +0800

    new bmi

--- PracLab2.py
+++ PracLab2.py
@@ -1,1 +1,2 @@
+
 print("ET0735 - (DevOps for AIoT) - PracLab 2 - Introduction to Python")
--- bmi.py
+++ bmi.py
@@ -2,13 +2,13 @@ def calculate_bmi(height, weight):
     bmi = weight/(height**2)
 
     if (bmi < 18.5):
-        return 'Under Weight', bmi
+        return -1
 
     elif (bmi >= 18.5 and bmi <=25.0):
-        return 'Normal Weight', bmi
+        return 0
 
     elif (bmi > 25.0):
-        return 'Over Weight', bmi
+        return 1
 
 height = float(input('Enter Height: '))
 weight = float(input('Enter Weight: '))
